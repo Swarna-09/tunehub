0d8f4906189a914096f027a20bd69d63ce01d660
Swarnajyothi <Swarnajyothi@LAPTOP-PULJOC1J>
2025-03-28 18:39:44 +0530

    1st Version of Tunehub

---
+++ src/main/java/com/example/demo/services/PlaylistServiceImplementation.java
@@ -0,0 +1,35 @@
+package com.example.demo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entities.Playlist;
+import com.example.demo.repositories.PlaylistRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class PlaylistServiceImplementation implements PlaylistService{
+	@Autowired
+    PlaylistRepository repo;
+	
+	
+	@Override
+	@Transactional
+	public Playlist addPlaylist(Playlist playlist) {
+		
+		return repo.save(playlist);
+		
+	}
+	@Override
+	public List<Playlist> fetchAllPlaylists() {
+		// TODO Auto-generated method stub
+		return repo.findAll();
+	}
+	
+
+
+}
